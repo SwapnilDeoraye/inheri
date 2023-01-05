23032cc69220664f2d4fd09148c158b78a7e6c09
SwapnilDeoraye <91518838+SwapnilDeoraye@users.noreply.github.com>
2023-01-05 13:05:15 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	abc swa = new abc();
+    swa.add();
+    }
+}
+class abc extends Main{
+    public void add(){
+        System.out.println("ok");
+    }
+}
